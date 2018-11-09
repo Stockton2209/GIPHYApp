161dbc23aae0bfa59153d475ab4166db43e82448
FightingSprrow <kbauertx@gmail.com>
2018-11-08 20:45:53 -0600

    functional JS app, needs css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,68 @@
 
 $(document).ready(function() {
 
-var buttonChoices = ["Cats", "Moar Cats", "Ryan Gosling", "Ready Player One", "Coding", "Cheesburgers", "Pizza",]
-// append an array of buttons at the top.
-for (var i = 0; i < buttonChoices.length; i++) {
-    $("#gifButtonArea").append("<button class='gifButtons' value="+ buttonChoices[i] + ">" + buttonChoices[i] + "</button>");
-}
+  var buttonChoices = ["Cats", "Moar Cats", "Ryan Gosling", "Ready Player One", "Coding", "Cheesburgers", "Pizza"];
 
-// Search field and search button for "Add an Animal"
-$("#searchDiv").append
-
-// Append HTML button to "gifButtonArea" after search butto is clicked.
-
-
-$(".gifButtons").on("click", function() {
-    // variable to hold the link for the API. api.giphy.com will show the syntax. 
-    // Attach API key property and value
-    // attach tag=cats
-    var gifSearch = $(this).val().trim();
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=1mwvVwbDJXA13TmnYXLJKAX0whsoKDHc&limit=2";
-    
-    // AJAX - API Query getter.
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-        var gifDiv = $("<div class='col-sm-3'>")
-        var rating = results[i].rating;
-
-        var p = $("<p>").text("Rating: " + rating);
-
-        var gifImage = $("<img class='img-fluid img-thumbnail'>").attr("src", results[i].images.fixed_height.url);
-        
-        gifDiv.prepend(p);
-        gifDiv.prepend(gifImage);
-
-        $("#gifs").prepend(gifDiv);
+  function renderButtons() {
+    $("#gifButtonArea").empty();
+    for (var i = 0; i < buttonChoices.length; i++) {
+        $("#gifButtonArea").append("<button class='gifButtons' value="+ buttonChoices[i] + ">" + buttonChoices[i] + "</button>");
         }
-      });
+        $(".gifButtons").on("click", populateGifs);
+  };
+  renderButtons();
+
+ 
+  function searchArea() {
+    $("#searchDiv").append("<input class='form-control mr-sm-2' type='text' placeholder='Search' aria-label='Search'></input><button class='btn btn-unique btn-rounded btn-sm my-0' id='searchBtn' type='submit'>Add Gif Button</button>");
+  }
+  searchArea();
+
+
+  $("#searchBtn").on("click", function() {
+    console.log("search button clicked")
+    var searchItem = $("#searchDiv > input").val();
+    buttonChoices.push(searchItem);
+    renderButtons();
   });
 
+
+  
+  function populateGifs() {
+    console.log(this);
+    var gifSearch = $(this).val().trim();
+    
+    var queryURL = "https://cors-ut-bootcamp.herokuapp.com/https://api.giphy.com/v1/gifs/random?tag=" + gifSearch + "&api_key=1mwvVwbDJXA13TmnYXLJKAX0whsoKDHc";
+    for (var i = 0; i < 1; i++) {
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        var gifDiv = $("<div>")
+        var gifData = response.data.images;
+        var gifImage = $("<img class='img-fluid img-thumbnail'>");
+        gifImage.attr("src", gifData.fixed_height_still.url);
+        gifImage.attr("data-still", gifData.fixed_height_still.url);
+        gifImage.attr("data-animate", gifData.fixed_height.url);
+        gifImage.attr("data-state", "still");
+        gifDiv.prepend(gifImage);
+        $("#gifs").prepend(gifDiv);
+      });
+    }
+  };
+
+  
+  $("#gifs > div > img").on("click", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr('src', $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      }
+    if (state === "animate") {
+      $(this).attr('src', $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  }); 
+        
 });
-
-// $("#gifs").on("click", function() {
-
-// FOR PAUSING AND STARTING GIFs
-// var state = $(this).attr("data-state");
-// if (state === "still") {
-//     $(this).attr('src', $(this).attr("data-animate"));
-//     $(this).attr("data-state", "animate")
-//   }
-//   if (state === "animate") {
-//     $(this).attr('src', $(this).attr("data-still"));
-//     $(this).attr("data-state", "still")
-//   }
-// });
